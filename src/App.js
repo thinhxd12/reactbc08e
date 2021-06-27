79729d11990b5604d9d9e068ebc815694c614a7f
unknown <thinhxd12@gmail.com>
2021-06-27 12:12:50 +0700

    BaiTapThucHanhLayout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import FunctionComponent from "./Components/FunctionComponent";
 import ClassComponent from "./Components/ClassComponent";
 import HomeComponent from "./Components/BaiTapLayoutComponent/HomeComponent";
 import Databinding from "./Databinding/Databinding";
+import BaiTapThucHanhLayout from "./BaiTapLayoutComponent/BaiTapThucHanhLayout/BaiTapThucHanhLayout";
 
 function App() {
   return (
     <div className="App">
       {/* <HomeComponent/> */}
-      <Databinding/>
+      {/* <Databinding/> */}
+      <BaiTapThucHanhLayout/>
     </div>
   );
 }
